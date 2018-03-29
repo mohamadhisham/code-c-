a16571f817d02e0e952b80dbebb763ba2f2dce63
mohamadhisham <36457354+mohamadhisham@users.noreply.github.com>
2018-03-29 20:06:00 +0200

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,26 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cin>>i;
+	switch(i)
+	{
+	case 10:
+			cout<<"i am in case 1";
+			break;
+
+	case 20:
+			cout<<"i am in case 2";
+			break;
+
+	case 30:
+			cout<<"i am in case 3";
+			break;
+	}
+return 0;
+}
+
+
+
+
