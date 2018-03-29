5d698cee9312388edca90b0a98e8749e1683e503
mohamadhisham <36457354+mohamadhisham@users.noreply.github.com>
2018-03-29 20:04:59 +0200

    Add files via upload

---
+++ marks.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	int mark;
+	cin>>mark;
+	if (mark>=90)
+		cout<<"a";
+	else if (mark>=80)
+		cout<<"b";
+	else if (mark>=70)
+		cout<<"c";
+	else if (mark>=60)
+		cout<<"d";
+	else
+		cout<<"f";
+return 0;
+}
